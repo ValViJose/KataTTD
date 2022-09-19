94f882cd6e1f910b8786c0c86a550c85cc455d70
78114845 <78114845@continental.edu.pe>
2022-09-19 18:24:47 -0500

    Plantillas de las clases del proyecto creadas

---
+++ tests/TestConjunto.py
@@ -0,0 +1,8 @@
+import unittest
+from src.logic.Conjunto import Conjunto
+
+class TestConjunto(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
