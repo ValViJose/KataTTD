b8dfd92cb4470c567500bf012bc0205908b5485f
78114845 <78114845@continental.edu.pe>
2022-09-19 18:40:21 -0500

    Test Conjunto Vacio

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -2,7 +2,8 @@ import unittest
 from src.logic.Conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto=Conjunto([])
+        self.assertIsNone(conjunto.promedio())
 
 
