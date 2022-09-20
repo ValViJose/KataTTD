06079b421bef2a6989136d9035811cdf01f724a7
78114845 <78114845@continental.edu.pe>
2022-09-19 19:11:38 -0500

    Caso 4 : Calcular Promedio con un conjunto de n Elementos (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -9,6 +9,10 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_unElemento_retornaValorUnicoElemento(self):
         conjunto = Conjunto([5])
         self.assertEqual(5,conjunto.promedio())
-    def test_conjunto_dosElementos_retornaValorUnicoElemento(self):
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
         conjunto = Conjunto([5,7])
         self.assertEqual(6,conjunto.promedio())
+
+    def test_conjunto_nElementos_retornaValorPromedioNElementos(self):
+        conjunto = Conjunto([2,4,8,10,15])
+        self.assertEqual((2+4+8+10+15)/6,conjunto.promedio())
